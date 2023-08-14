2e35e04364245c30ab3870666685415e054732e9
cany <couleurvide1008@gmail.com>
2023-08-14 14:45:48 +0900

    スプラッシュ画面を作成

---
+++ src/components/Splash.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+// アプリを開いた時の初期画面
+
+const Splash = () => {
+  return (
+    <div className="splash">
+      <h1>きょう何食べる？</h1>
+
+      <img src="./dinner.jpg" alt="" />
+    </div>
+  );
+
+  // 入りと終わりにアニメーションをつける
+};
+
+export default Splash;
