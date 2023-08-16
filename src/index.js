ac9b956ff56c3d3391f2bc6b7a85653231deea38
cany <couleurvide1008@gmail.com>
2023-08-16 16:22:42 +0900

    ルーティング追加

--- src/index.js
+++ src/index.js
@@ -2,11 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./reset.css";
 import "./App.scss";
-import App from "./App";
+import Splash from "./components/Splash";
+import QuestionPage from "./components/QuestionPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Splash />} />
+        <Route path="/:id" element={<QuestionPage />} />
+        {/* <Route path="result" element={<Result />} /> */}
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
