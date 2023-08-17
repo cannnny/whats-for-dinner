1531a263e250e88b90d16b9a80dfc5a49d6c365a
cany <couleurvide1008@gmail.com>
2023-08-17 13:31:23 +0900

    コンポーネントを自動で切り替える機能を実装

--- src/components/Splash.jsx
+++ src/components/Splash.jsx
@@ -1,15 +1,26 @@
-import React from "react";
+import { React, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 // アプリを開いた時の初期画面
-
+// useEffect + setTimeout + useNavigate
 const Splash = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigate("1");
+    }, 3000);
+    return () => {
+      clearTimeout(timer);
+    };
+  });
+
   return (
     <div className="splash">
       <h1>きょう何食べる？</h1>
       <img src="./dinner.jpg" alt="" />
     </div>
   );
-
   // 入りと終わりにアニメーションをつける
 };
 
